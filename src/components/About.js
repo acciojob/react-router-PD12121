8e90612374d40bc1d0583ac833440bc7b443f35f
pd212 <deshpandepranav47@gmail.com>
2023-07-29 17:36:14 +0530

    done

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <div>
+        <h1>Abour</h1>
+        <p>This the a simple React Router program.</p>
+    </div>
+  )
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
 
 import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
 import './../styles/App.css';
+import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+
+        </Routes>
+      </div>
     </div>
   )
-}
+};
 
-export default App
+export default App;
